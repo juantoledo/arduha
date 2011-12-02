3dd6cd6a812d576343b95b52c28812002dae6a29
Juan Toledo Carrasco <juantoledo@gmail.com>
2011-12-02 16:56:23 -0300

    Added Images

--- src/www/index.php
+++ src/www/index.php
@@ -1,7 +1,9 @@
-<!DOCTYPE html> 
+<?php
+include_once("release.php");
+?><!DOCTYPE html> 
 <html> 
 	<head> 
-	<title>Open HA</title> 
+	<title>Arduha</title> 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<link rel="stylesheet" href="css/jquery/jquery.mobile-1.0.css" />
 	<script type="text/javascript" src="js/jquery/jquery-1.7.1.js"></script>
@@ -9,40 +11,46 @@
 </head> 
 <body> 
 
-<div data-role="page">
+<div data-role="page" data-fullscreen="true">
 
 	<div data-role="header">
-		<h1>OpenHA Control Panel</h1>
+		Arduha Control Panel v<?php echo VERSION; ?>
 	</div><!-- /header -->
 
-	<div data-role="content" data-theme="b" data-content-theme="b">	
+	<div data-role="content" data-theme="b" data-content-theme="b" >	
 		<div data-role="collapsible"  data-theme="b" data-collapsed="false" data-content-theme="c">
-			<h3>Sensors</h3>
+			<h3>Sensors <img style="padding-left:10px; padding-bottom:10px;vertical-align:middle;" src="images/sen.png"/></h3>
 			<p>
 				<div class="ui-grid-b">
-					<div class="ui-block-a">Block A</div>
-					<div class="ui-block-b">Block B</div>
-					<div class="ui-block-c">Block C</div>
+					<div class="ui-block-a">Arduino Thermometer</div>
+					<div class="ui-block-b">Main Door Magneto</div>
+					<div class="ui-block-c">Kitchen Magneto</div>
+				</div>
+				<p></p>
+				<div class="ui-grid-b">
+					<div class="ui-block-a">Living Room PIR</div>
+					<div class="ui-block-b"></div>
+					<div class="ui-block-c"></div>
 				</div>
 			</p>
 		</div>
 		<div data-role="collapsible" data-theme="b" data-collapsed="false" data-content-theme="c">
-			<h3>Alarms</h3>
+			<h3>Alarms / Actuators <img style="padding-left:10px; padding-bottom:10px;vertical-align:middle;" src="images/alarm.png"/> <img style="padding-left:10px; padding-bottom:10px;vertical-align:middle;" src="images/act.png"/></h3>
 			<p>				
 			<div class="ui-grid-b">
-					<div class="ui-block-a">Block A</div>
-					<div class="ui-block-b">Block B</div>
-					<div class="ui-block-c">Block C</div>
+					<div class="ui-block-a">Backyard Main Light</div>
+					<div class="ui-block-b">Bedroom Light</div>
+					<div class="ui-block-c">Embedded Light</div>
 			</div>
 			</p>
 		</div>
 				<div data-role="collapsible" data-theme="b" data-collapsed="false" data-content-theme="c">
-			<h3>General Status</h3>
+			<h3>General Status <img style="padding-left:10px; padding-bottom:10px;vertical-align:middle;" src="images/general.png"/></h3>
 			<p>		
 				<div class="ui-grid-b">
-					<div class="ui-block-a">Block A</div>
-					<div class="ui-block-b">Block B</div>
-					<div class="ui-block-c">Block C</div>
+					<div class="ui-block-a">House Status ARMED</div>
+					<div class="ui-block-b"></div>
+					<div class="ui-block-c"></div>
 				</div>
 			</p>
 		</div>
---
+++ src/www/release.php
@@ -0,0 +1,5 @@
+<?php
+
+define("VERSION", "0.1");
+
+?>
