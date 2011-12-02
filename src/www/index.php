412f89f72dff73108ab4b3b0677152c315cfcb2e
Juan Toledo Carrasco <juantoledo@gmail.com>
2011-12-02 15:44:47 -0300

    Prototype App

---
+++ src/www/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+	<title>Open HA</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="css/jquery/jquery.mobile-1.0.css" />
+	<script type="text/javascript" src="js/jquery/jquery-1.7.1.js"></script>
+	<script type="text/javascript" src="js/jquery/jquery.mobile-1.0.js"></script>
+</head> 
+<body> 
+
+<div data-role="page">
+
+	<div data-role="header">
+		<h1>OpenHA Control Panel</h1>
+	</div><!-- /header -->
+
+	<div data-role="content" data-theme="b" data-content-theme="b">	
+		<div data-role="collapsible"  data-theme="b" data-collapsed="false" data-content-theme="c">
+			<h3>Sensors</h3>
+			<p>
+				<div class="ui-grid-b">
+					<div class="ui-block-a">Block A</div>
+					<div class="ui-block-b">Block B</div>
+					<div class="ui-block-c">Block C</div>
+				</div>
+			</p>
+		</div>
+		<div data-role="collapsible" data-theme="b" data-collapsed="false" data-content-theme="c">
+			<h3>Alarms</h3>
+			<p>				
+			<div class="ui-grid-b">
+					<div class="ui-block-a">Block A</div>
+					<div class="ui-block-b">Block B</div>
+					<div class="ui-block-c">Block C</div>
+			</div>
+			</p>
+		</div>
+				<div data-role="collapsible" data-theme="b" data-collapsed="false" data-content-theme="c">
+			<h3>General Status</h3>
+			<p>		
+				<div class="ui-grid-b">
+					<div class="ui-block-a">Block A</div>
+					<div class="ui-block-b">Block B</div>
+					<div class="ui-block-c">Block C</div>
+				</div>
+			</p>
+		</div>
+	</div><!-- /content -->
+
+</div><!-- /page -->
+
+</body>
+</html>
+
+
+
